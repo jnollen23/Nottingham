2ce919de7d49c36ac89ed89022bb51cf2b76ec6f
Jacob Nollen <jacob.nollen@netplanner.com>
2023-02-06 20:10:38 -0500

    added server.js to have something to deploy for heroku

---
+++ server.js
@@ -0,0 +1,10 @@
+require("dotenv").config;
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.listen(PORT, () => console.log("Now Listneing"));
