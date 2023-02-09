bb87006d72abbaed93352646e762a21e4a7c3e1b
nicholas4418 <nicholas4418@gmail.com>
2023-02-09 18:34:32 -0500

    Added portfolio handlebar and controllers, edited server.js, not sure if needed

--- server.js
+++ server.js
@@ -1,12 +1,26 @@
 require("dotenv").config;
 const express = require('express');
+const exphbs = require('express-handlebars');
+const routes = require('./controllers');
+const path = require('path');
+const sequelize = require('./configuration/config');
+
 const app = express();
 const stock = require('./Utils/stockmarket');
 
+
 const PORT = process.env.APP_PORT || 3001;
+
+
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handelbars');
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(routes);
 
 //app.listen(PORT, () => console.log("Now Listneing"));
 
