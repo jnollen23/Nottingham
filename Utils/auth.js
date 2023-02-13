9b7d711683dca28793c34ef85ac6945f31f7f071
Jacob Nollen <jacob.nollen@netplanner.com>
2023-02-12 23:14:33 -0500

    updated to add a comment to auth about what goes in the if statement

--- Utils/auth.js
+++ Utils/auth.js
@@ -1,9 +1,11 @@
 const withAuth = (req, res, next) => {
-  if (!req.session.logged_in && 
-    !(req.path.includes('login') || 
-    req.path.includes('signup') || 
-    req.path === '/')) {
-      res.redirect('/login');
+  if (!req.session.logged_in &&
+    !(//Any pages added to the site that should not be authenicated should be added to this list
+      req.path.includes('login') ||
+      req.path.includes('signup') ||
+      req.path === '/'
+    )) {
+    res.redirect('/login');
   } else {
     next();
   }
