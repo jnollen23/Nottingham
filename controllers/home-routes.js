795be46795b1bc7b977bba808c7b7506eabac0d8
nicholas4418 <nicholas4418@gmail.com>
2023-02-14 14:50:11 -0500

    DO NOT MERGE, added carousel and chart.js

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -39,6 +39,21 @@ router.get("/dashboard", withAuth, async (req, res) => {
   }
 });
 
+router.get("/portfolio", withAuth, async (req, res) => {
+  try {
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ["password"] },
+    });
+    const user = userData.get({ plain: true});
+    res.render("portfolio", {
+      user,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+})
+
 router.get("/login", (req, res) => {
   if (req.session.logged_in) {
     res.redirect("/dashboard");
