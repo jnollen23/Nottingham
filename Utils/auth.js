bcc53e35582e8648d9fc0c78247e09eae0dbb98a
Jacob Nollen <jacob.nollen@netplanner.com>
2023-02-14 19:23:49 -0500

    signup was still refering to the login page and not signup, now when you hit signup it takes you to sign up and not login

--- Utils/auth.js
+++ Utils/auth.js
@@ -3,9 +3,10 @@ const withAuth = (req, res, next) => {
     !(//Any pages added to the site that should not be authenicated should be added to this list
       req.path.includes('login') ||
       req.path.includes('signup') ||
-      req.path === '/'
+      req.path === '/' ||
+      req.path.includes('images')
     )) {
-    res.redirect('/login');
+    res.redirect('/');
   } else {
     next();
   }
