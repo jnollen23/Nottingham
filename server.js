761758a5d29874390d9a7847d8a23b876eb1bb64
Jacob Nollen <jacob.nollen@netplanner.com>
2023-02-15 23:52:31 -0500

    forgot to save the last change to utils in server js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const routes = require('./controllers');
 const sequelize = require('./configuration/config');
-const withAuth = require('./utils/auth');
+const withAuth = require('./Utils/auth');
 
 const app = express();
 const PORT = process.env.APP_PORT || 3001;
